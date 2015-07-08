2f0c9e5d735d88bf5fb7e445a0d3bd226756482e
Sean Joyal <sean.joyal@gmail.com>
2015-07-08 16:37:19 -0400

    restructured js files, naming conventions, added html views, included scripts in index.html

--- app/scripts/iron-pong.js
+++ app/scripts/iron-pong.js
@@ -1,1 +1,36 @@
-console.log('\'Allo \'Allo!');
+(function(){
+
+  angular.module('iron-pong', ['ngRoute'])
+
+    .config(function($routeProvider){
+      $routeProvider.when('/leaderboard', {
+        templateUrl: 'views/leaderboard.html',
+        // controller: 'LeaderboardController',
+        // controllerAs: 'leaderboard'
+      }); // END $routeProvider leaderboard
+
+      $routeProvider.when('/gameresults/:gameresult', {
+        templateUrl: 'views/gameresults.html',
+        // controller: 'GameResultController',
+        // controllerAs: 'gameresult'
+      }); // END $routeProvider gameresults
+
+      $routeProvider.when('/submit', {
+        templateUrl: 'views/submitgameresult.html',
+        // controller: 'SubmitGameResutController',
+        // controllerAs: 'submitgameresult'
+      }); // END $routeProvider submitgameresult
+
+      $routeProvider.when('/', {
+        redirectTo: '/leaderboard'
+      }); // END redirecTo leaderboard
+
+      $routeProvider.otherwise('/404', {
+        templateUrl: 'views/404.html'
+      });
+    }); // END otherwise 404
+
+    .controller('MainController', function($scope) {
+
+    }); // END MainController
+})();
