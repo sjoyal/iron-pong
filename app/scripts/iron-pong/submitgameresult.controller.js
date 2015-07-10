bf4e105d73eb90f027202e1e578cfa0448060ee8
Sean Joyal <sean.joyal@gmail.com>
2015-07-10 13:51:00 -0400

    working with ng-model and ng-change to restrict options on second set of select tags for submitting game results

--- app/scripts/iron-pong/submitgameresult.controller.js
+++ app/scripts/iron-pong/submitgameresult.controller.js
@@ -4,6 +4,11 @@
 
     .controller('SubmitController', function($scope, $http){
       $scope.players = [ ];
+      $scope.competitors = [ ];
+      $scope.addPlayers = function(playerName){
+        $scope.competitors.push(playerName);
+        console.log($scope.competitors);
+      };
       // Retrieve list of stargazers from cohort repo
       $http.get('api/github/repos/theironyard--orlando/2015--summer--fee/stargazers/stargazers.json')
         .then(function(response){
