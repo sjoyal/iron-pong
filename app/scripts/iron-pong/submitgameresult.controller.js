2399ae1e1b46a9b8f34ad223ddf89337ea332880
Sean Joyal <sean.joyal@gmail.com>
2015-07-10 11:45:23 -0400

    retrieving cohort repo stargazers with .get() request, assigning to players within SubmitController

--- app/scripts/iron-pong/submitgameresult.controller.js
+++ app/scripts/iron-pong/submitgameresult.controller.js
@@ -2,8 +2,14 @@
 (function(){
   angular.module('iron-pong')
 
-    .controller('SubmitController', function($http){
-      console.log('submitshit');
+    .controller('SubmitController', function($scope, $http){
+      $scope.players = [ ];
+      // Retrieve list of stargazers from cohort repo
+      $http.get('api/github/repos/theironyard--orlando/2015--summer--fee/stargazers/stargazers.json')
+        .then(function(response){
+          players = response.data;
+          console.log(players[0].login);
+        });
     });
 
 
