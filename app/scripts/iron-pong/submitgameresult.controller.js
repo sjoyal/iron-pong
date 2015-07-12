a794ed03d48020fd0ed5050055ffd7132023abf5
Sean Joyal <sean.joyal@gmail.com>
2015-07-12 17:34:01 -0400

    reverted to cached data for the development process on submit result controller

--- app/scripts/iron-pong/submitgameresult.controller.js
+++ app/scripts/iron-pong/submitgameresult.controller.js
@@ -13,14 +13,13 @@
         $scope.competitors[1] = playerName;
         console.log($scope.competitors);
       };
-      debugger;
       // Retrieve list of stargazers from cohort repo
       $scope.authInfo = Auth.authStatus();
-      // $http.get('api/github/repos/theironyard--orlando/2015--summer--fee/stargazers/stargazers.json')
-      $http.get('https://api.github.com/repos/TheIronYard--Orlando/2015--SUMMER--FEE/stargazers?access_token=' + $scope.authInfo.github.accessToken)
+      $http.get('api/github/repos/theironyard--orlando/2015--summer--fee/stargazers/stargazers.json')
+      // $http.get('https://api.github.com/repos/TheIronYard--Orlando/2015--SUMMER--FEE/stargazers?access_token=' + $scope.authInfo.github.accessToken)
         .then(function(response){
           $scope.players = response.data;
-          console.log($scope.players); 
+          console.log($scope.players);
         });
     });
 
