89356f09d24514d8bf82d15c4d98b2be78396a83
Sean Joyal <sean.joyal@gmail.com>
2015-07-12 23:43:36 -0400

    removed  request from players factory, delay data being reflected in view

--- app/scripts/iron-pong/submitgameresult.controller.js
+++ app/scripts/iron-pong/submitgameresult.controller.js
@@ -2,25 +2,18 @@
 (function(){
   angular.module('iron-pong')
 
-    .controller('SubmitController', function($scope, $http, Auth, Players){
-      $scope.players = Players.retrievePlayers();
-      // $scope.competitors = [ ];
-      // $scope.addPlayer1 = function(playerName){
-      //   $scope.competitors[0] = playerName;
-      //   console.log($scope.competitors);
-      // };
-      // $scope.addPlayer2 = function(playerName){
-      //   $scope.competitors[1] = playerName;
-      //   console.log($scope.competitors);
-      // };
+    .controller('SubmitController', function($scope, $http, Players){
+      // $scope.players = Players.retrievePlayers();
+      $scope.players = [];
       // Retrieve list of stargazers from cohort repo
       // $scope.authInfo = Auth.authStatus();
-      // $http.get('api/github/repos/theironyard--orlando/2015--summer--fee/stargazers/stargazers.json')
-      // // $http.get('https://api.github.com/repos/TheIronYard--Orlando/2015--SUMMER--FEE/stargazers?access_token=' + $scope.authInfo.github.accessToken)
-      //   .then(function(response){
-      //     $scope.players = response.data;
-      //     console.log($scope.players);
-      //   });
+      $http.get('api/github/repos/theironyard--orlando/2015--summer--fee/stargazers/stargazers.json')
+      // $http.get('https://api.github.com/repos/TheIronYard--Orlando/2015--SUMMER--FEE/stargazers?access_token=' + $scope.authInfo.github.accessToken)
+        .then(function(response){
+          $scope.players = response.data;
+          console.log($scope.players);
+        });
+      $scope.results = [ ];
       $scope.gameresult = {
         winner: '',
         winnerScore: '',
@@ -29,6 +22,26 @@
         summary: '',
         created: ''
       };
-      $scope.results = [ ];
+      $scope.submitResults = function(){
+        $scope.results.push($scope.gameresult);
+        $scope.gameresult = {
+          winner: '',
+          winnerScore: '',
+          loser: '',
+          loserScore: '',
+          summary: '',
+          created: ''
+        }
+      };
     });
 })();
+
+// $scope.competitors = [ ];
+// $scope.addPlayer1 = function(playerName){
+//   $scope.competitors[0] = playerName;
+//   console.log($scope.competitors);
+// };
+// $scope.addPlayer2 = function(playerName){
+//   $scope.competitors[1] = playerName;
+//   console.log($scope.competitors);
+// };
