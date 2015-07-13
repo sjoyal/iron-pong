b78a133391219e855a459c310c2006553573b654
Sean Joyal <sean.joyal@gmail.com>
2015-07-12 22:57:54 -0400

    refactored retrieve players into a service

--- app/scripts/iron-pong/services/results.service.js
+++ app/scripts/iron-pong/services/results.service.js
@@ -15,6 +15,22 @@
           ref.unauth();
           console.log('goodbye');
         }
-      };
-    });
+      } // END 'Auth' return
+    }) // END Auth factory
+    .factory('Players', function(Auth, $http){
+      var authInfo = Auth.authStatus();
+      this.players = [];
+      var self = this;
+      return {
+        retrievePlayers: function(){
+          $http.get('api/github/repos/theironyard--orlando/2015--summer--fee/stargazers/stargazers.json')
+          // $http.get('https://api.github.com/repos/TheIronYard--Orlando/2015--SUMMER--FEE/stargazers?access_token=' + $scope.authInfo.github.accessToken)
+          .then(function(response){
+            self.players = response.data;
+          });
+          return self.players;
+        }
+      }
+    })
+    ; // END ALL THE THINGS!
 })();
--- app/scripts/iron-pong/submitgameresult.controller.js
+++ app/scripts/iron-pong/submitgameresult.controller.js
@@ -2,7 +2,25 @@
 (function(){
   angular.module('iron-pong')
 
-    .controller('SubmitController', function($scope, $http, Auth){
+    .controller('SubmitController', function($scope, $http, Auth, Players){
+      $scope.players = Players.retrievePlayers();
+      // $scope.competitors = [ ];
+      // $scope.addPlayer1 = function(playerName){
+      //   $scope.competitors[0] = playerName;
+      //   console.log($scope.competitors);
+      // };
+      // $scope.addPlayer2 = function(playerName){
+      //   $scope.competitors[1] = playerName;
+      //   console.log($scope.competitors);
+      // };
+      // Retrieve list of stargazers from cohort repo
+      // $scope.authInfo = Auth.authStatus();
+      // $http.get('api/github/repos/theironyard--orlando/2015--summer--fee/stargazers/stargazers.json')
+      // // $http.get('https://api.github.com/repos/TheIronYard--Orlando/2015--SUMMER--FEE/stargazers?access_token=' + $scope.authInfo.github.accessToken)
+      //   .then(function(response){
+      //     $scope.players = response.data;
+      //     console.log($scope.players);
+      //   });
       $scope.gameresult = {
         winner: '',
         winnerScore: '',
@@ -11,26 +29,6 @@
         summary: '',
         created: ''
       };
-      $scope.players = [ ];
-      $scope.competitors = [ ];
-      $scope.addPlayer1 = function(playerName){
-        $scope.competitors[0] = playerName;
-        console.log($scope.competitors);
-      };
-      $scope.addPlayer2 = function(playerName){
-        $scope.competitors[1] = playerName;
-        console.log($scope.competitors);
-      };
-      // Retrieve list of stargazers from cohort repo
-      $scope.authInfo = Auth.authStatus();
-      $http.get('api/github/repos/theironyard--orlando/2015--summer--fee/stargazers/stargazers.json')
-      // $http.get('https://api.github.com/repos/TheIronYard--Orlando/2015--SUMMER--FEE/stargazers?access_token=' + $scope.authInfo.github.accessToken)
-        .then(function(response){
-          $scope.players = response.data;
-          console.log($scope.players);
-        });
-
+      $scope.results = [ ];
     });
-
-
 })();
