74db9df7008d9bc9c221d524760797c7341149a7
Sean Joyal <sean.joyal@gmail.com>
2015-07-14 03:45:42 -0400

    using  to pull in the game object associated with the selected gameresultID

--- app/scripts/iron-pong/gameresult.controller.js
+++ app/scripts/iron-pong/gameresult.controller.js
@@ -3,11 +3,11 @@
   'use strict';
 
   angular.module('iron-pong')
-    .controller('GameResultController', function ($scope, $firebase, $firebaseArray, $stateParams){
+    .controller('GameResultController', function ($scope, $firebase, $firebaseObject, $stateParams){
 
       // pull in specific game result from
-      var game = new Firebase('https://iron-pong.firebaseio.com/gameresults/' + $stateParams.gameresultID);
-      $scope.result = $firebaseObject(game);
+      var game = new Firebase('https://iron-pong.firebaseio.com/gameresults/');
+      $scope.result = $firebaseObject(game.child($stateParams.gameresultID));
       console.log($scope.result);
 
     }); // END GameResultController
