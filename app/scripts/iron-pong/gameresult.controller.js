c1db5010f781399ebc032984f2ecb8886f9b256d
Sean Joyal <sean.joyal@gmail.com>
2015-07-14 02:58:18 -0400

    adding game result controller, wiring up w/ injection of  to display clicked game

--- app/scripts/iron-pong/gameresult.controller.js
+++ app/scripts/iron-pong/gameresult.controller.js
@@ -1,1 +1,11 @@
+/* global angular Firebase */
+(function(){
+  'use strict';
 
+  angular.module('iron-pong')
+    .controller('GameResultController', function ($scope, $firebase, $firebaseArray, $stateParams){
+       console.log($stateParams);
+      // pull in specific game result from
+
+    }); // END GameResultController
+})();
