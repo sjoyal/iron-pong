39385eb2acbd17e4ac858d30c0e0f34287954dc0
Sean Joyal <sean.joyal@gmail.com>
2015-07-15 18:20:56 -0400

    setting up tabs with ng-click ng-show and ng-hide with angular

--- app/scripts/iron-pong/leaderboard.controller.js
+++ app/scripts/iron-pong/leaderboard.controller.js
@@ -37,26 +37,15 @@
           });
         });
 
+        this.tab = 1;
+        this.selectTab = function(setTab){
+          this.tab = setTab;
+        };
+        this.isSelected = function(checkTab){
+          return this.tab === checkTab;
+        };
         console.log($scope.leaderboard);
-
-        // FIXME: Pull and munge data for live stuff in the leaderboardz
-        // $scope.players = [
-        //   {avatar_url: 'http://lorempixel.com/120/120/people/1', login: 'sjoyal',
-        //   wins: 55, losses: 14, pct: .757, games: 69},
-        //   {avatar_url: 'http://lorempixel.com/120/120/people/2', login: 'pcreglow',
-        //   wins: 40, losses: 30, pct: .568, games: 70},
-        //   {avatar_url: 'http://lorempixel.com/120/120/people/3', login: 'jorgehjr84',
-        //   wins: 8, losses: 40, pct: .213, games: 48},
-        //   {avatar_url: 'http://lorempixel.com/120/120/people/5', login: 'gatorpazz',
-        //   wins: 0, losses: 0, pct: .000, games: 0},
-        //   {avatar_url: 'http://lorempixel.com/120/120/people/6', login: 'jessyriordan',
-        //   wins: 0, losses: 0, pct: .000, games: 0},
-        //   {avatar_url: 'http://lorempixel.com/120/120/people/7', login: 'mstaehling',
-        //   wins: 0, losses: 0, pct: .000, games: 0},
-        //   {avatar_url: 'http://lorempixel.com/120/120/people/4', login: 'al-the-x',
-        //   wins: 0, losses: 125, pct: .000, games: 125}
-        // ] // END players
-
+        
         // $scope.tableParams = new ngTableParams({
         //   sorting: { gamesWonLength: 'desc' }  // initial sorting
         // }, {
@@ -67,6 +56,26 @@
         //   }
         // });
       }); // END LeaderboardController
-
-
 })();
+
+
+
+
+
+
+// $scope.players = [
+//   {avatar_url: 'http://lorempixel.com/120/120/people/1', login: 'sjoyal',
+//   wins: 55, losses: 14, pct: .757, games: 69},
+//   {avatar_url: 'http://lorempixel.com/120/120/people/2', login: 'pcreglow',
+//   wins: 40, losses: 30, pct: .568, games: 70},
+//   {avatar_url: 'http://lorempixel.com/120/120/people/3', login: 'jorgehjr84',
+//   wins: 8, losses: 40, pct: .213, games: 48},
+//   {avatar_url: 'http://lorempixel.com/120/120/people/5', login: 'gatorpazz',
+//   wins: 0, losses: 0, pct: .000, games: 0},
+//   {avatar_url: 'http://lorempixel.com/120/120/people/6', login: 'jessyriordan',
+//   wins: 0, losses: 0, pct: .000, games: 0},
+//   {avatar_url: 'http://lorempixel.com/120/120/people/7', login: 'mstaehling',
+//   wins: 0, losses: 0, pct: .000, games: 0},
+//   {avatar_url: 'http://lorempixel.com/120/120/people/4', login: 'al-the-x',
+//   wins: 0, losses: 125, pct: .000, games: 125}
+// ] // END players
