848e4233e9a067ad6a63b53130a9a09b9078f944
Sean Joyal <sean.joyal@gmail.com>
2015-07-17 12:23:18 -0400

    delete method updating player stats for winner as appropriate

--- app/scripts/iron-pong/gameresult.controller.js
+++ app/scripts/iron-pong/gameresult.controller.js
@@ -20,9 +20,22 @@
       this.deleteGame = function(){
         Restangular.one('gameresults', $stateParams.gameresultID).remove()
           .then(function(data){
-            console.log(data);
+            Restangular.one('players', self.game.winner.login).get()
+              .then(function(data){
+                if (data.gamesPlayed === 1) {
+                  Restangular.one('players', self.game.winner.login).remove();
+                } else {
+                  Restangular.one('players/' + self.game.winner.login).patch({
+                    wins: (data.wins - 1),
+                    gamesPlayed: (data.gamesPlayed - 1)
+                  }).then(function(data){
+                    // Restangular.one('players', winner).post('games', {jumanji});
+                  })
+                }
+
+            });
           });
-      };
+        };
 
     }); // END GameResultController
 })();
