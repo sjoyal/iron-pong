3aec960ab7cd7cd24b78e4ab40e431fc212605ab
Sean Joyal <sean.joyal@gmail.com>
2015-07-17 21:11:07 -0400

    adding player data to player page using Restangular get request and  to retrieve player data

--- app/scripts/iron-pong/playerpage.controller.js
+++ app/scripts/iron-pong/playerpage.controller.js
@@ -4,5 +4,14 @@
   angular.module('iron-pong')
     .controller('PlayerPageController', function($scope, $stateParams, Restangular){
       console.log($stateParams);
+      this.athlete = {};
+      this.athleteDetails = {};
+      var self = this;
+      Restangular.one('players', $stateParams.playerID).get()
+        .then(function(data){
+          self.athlete = data.plain();
+          self.athlete.username = $stateParams.playerID;
+          console.log(self.athlete);
+        });
     }); // END PlayerPageController
 })();
