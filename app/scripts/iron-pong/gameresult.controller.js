b23f4051e0b92802cc3393211aa8c209d60940ae
Sean Joyal <sean.joyal@gmail.com>
2015-07-22 10:48:21 -0400

    comment styling looking good, removed refactored code from game result controller

--- app/scripts/iron-pong/gameresult.controller.js
+++ app/scripts/iron-pong/gameresult.controller.js
@@ -24,33 +24,7 @@
         var loser = self.game.loser.login;
         Restangular.one('gameresults', $stateParams.gameresultID).remove()
           .then(function(){
-            // Restangular.one('players', winner).get()
-            //   .then(function(gameWinner){
-            //     if (gameWinner.gamesPlayed === 1) {
-            //       Restangular.one('players', winner).remove();
-            //     } else {
-            //       Restangular.one('players/' + winner).patch({
-            //         wins: (gameWinner.wins - 1),
-            //         gamesPlayed: (gameWinner.gamesPlayed - 1)
-            //       }).then(function(){
-            //         // Remove game reference under player ID
-            //       });
-            //     }
-            //   });
             Delete.resultRemove(winner, 1, 0);
-            // Restangular.one('players', loser).get()
-            //   .then(function(gameLoser){
-            //     if (gameLoser.gamesPlayed === 1) {
-            //       Restangular.one('players', loser).remove();
-            //     } else {
-            //       Restangular.one('players/' + loser).patch({
-            //         losses: (gameLoser.losses - 1),
-            //         gamesPlayed: (gameLoser.gamesPlayed - 1)
-            //       }).then(function(){
-            //         // Remove game reference under player ID
-            //       });
-            //     }
-            //   });
             Delete.resultRemove(loser, 0, 1);
           });
         };
